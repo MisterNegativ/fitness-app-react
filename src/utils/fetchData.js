cabc092e560e1c6675179adf41a4bf6cd23ebd72
MisterNegativ <120665517+MisterNegativ@users.noreply.github.com>
2023-12-14 23:10:21 +0600

    Новый компонент ExerciseDetail

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
     },
 };
 
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ff33ae0779msh2be7dc5cae73e3fp193e9ajsnfe199ebf34b7',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    },
+  };
+
 export const fetchData = async (url, options) => {
     const response =  await fetch(url, options);
     const data = await response.json();
