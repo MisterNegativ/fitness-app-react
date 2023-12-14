be15c9143992d85d3b943e86236d21c6717dd075
MisterNegativ <120665517+MisterNegativ@users.noreply.github.com>
2023-12-14 22:53:09 +0600

    Компоненты Exercises и Card, исправление багов

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -4,7 +4,7 @@ import { Box, Stack, Typography } from '@mui/material';
 import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
-  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+  (<Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
@@ -20,7 +20,7 @@ const HeroBanner = () => (
       Exercise
     </Typography>
     <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
-  </Box>
+  </Box>)
 );
 
 export default HeroBanner;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,9 @@
 export const exerciseOptions = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': 'ff33ae0779msh2be7dc5cae73e3fp193e9ajsnfe199ebf34b7'
-  },
+  method: 'GET',  
+    headers: {
+        'X-RapidAPI-Key': 'ff33ae0779msh2be7dc5cae73e3fp193e9ajsnfe199ebf34b7',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    },
 };
 
 export const fetchData = async (url, options) => {
